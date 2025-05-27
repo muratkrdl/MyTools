fe9e86e877460060aed23780f25c3c9bc543ce6e
Murat KIRDAL <muratkirdal0101@gmail.com>
2025-05-27 11:10:40 +0300

    MonoSingleton

---
+++ Assets/_Scripts/Singleton/MonoSingleton.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+
+namespace _Scripts.Singleton
+{
+    public class MonoSingleton<T> : MonoBehaviour where T : MonoBehaviour
+    {
+        [SerializeField] private bool dontDestroyOnLoad;
+        
+        private static T _instance;
+
+        public static T Instance
+        {
+            get
+            {
+                if (_instance) return _instance;
+                
+                _instance = FindAnyObjectByType<T>();
+                if (_instance) return _instance;
+                
+                GameObject obj = new GameObject(typeof(T).Name);
+                _instance = obj.AddComponent<T>();
+
+                return _instance;
+            }
+        }
+
+        protected virtual void Awake()
+        {
+            var instance = Instance;
+
+            if (_instance != this)
+            {
+                Destroy(gameObject);
+                return;
+            }
+
+            if (dontDestroyOnLoad)
+            {
+                DontDestroyOnLoad(gameObject);
+            }
+        }
+        
+        protected virtual void OnDestroy()
+        {
+            if (_instance == this)
+            {
+                _instance = null;
+            }
+        }
+        
+    }
+}
